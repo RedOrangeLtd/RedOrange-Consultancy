4236b32c922e720157bcadb96520eb9fd52a69ef
Moinul Islam <moinulilm10@gmail.com>
2025-09-09 09:33:16 +0600

    `Removed Link component and href properties from WhatWeDO.js component`

--- app/components/WhatWeDO.js
+++ app/components/WhatWeDO.js
@@ -1,10 +1,9 @@
 import { sliderProps } from "@/utility/sliderProps";
-import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 const projects = [
   {
-    href: "work",
+    // href: "work",
     image: "/img/what_we_do/what_we_do_1.webp",
     title: "Conceptualization & Project Design",
     subtitle: "Shaping ideas into impactful solutions",
@@ -16,25 +15,25 @@ const projects = [
     subtitle: "Laying the foundation for success",
   },
   {
-    href: "work",
+    // href: "work",
     image: "/img/what_we_do/what_we_do_3.webp",
     title: "Implementation & Coordination Support",
     subtitle: "Turning plans into real-world action",
   },
   {
-    href: "work",
+    // href: "work",
     image: "/img/what_we_do/what_we_do_4.webp",
     title: "Research, Monitoring, Evaluation & Learning (MEL)",
     subtitle: "Driving impact through knowledge",
   },
   {
-    href: "work",
+    // href: "work",
     image: "/img/what_we_do/what_we_do_5.webp",
     title: "Policy Engagement & Sustainability",
     subtitle: "Building pathways for long-term change",
   },
   {
-    href: "work",
+    // href: "work",
     image: "/img/what_we_do/what_we_do_6.webp",
     title: "Creating Business Opportunities",
     subtitle: "Empowering growth and innovation",
@@ -42,8 +41,11 @@ const projects = [
 ];
 
 // Reusable card
-const ProjectCard = ({ href, image, title, subtitle }) => (
-  <Link href={href} className="mil-project-card mil-land">
+const ProjectCard = ({ image, title, subtitle }) => (
+  <div
+    // href={href}
+    className="mil-project-card mil-land"
+  >
     <div className="mil-cover-frame mil-up">
       <div className="mil-hover-frame">
         <img
@@ -60,7 +62,7 @@ const ProjectCard = ({ href, image, title, subtitle }) => (
       <h4 className="mil-mb-10">{title}</h4>
       <p>{subtitle}</p>
     </div>
-  </Link>
+  </div>
 );
 
 const WhatWeDO = () => {
